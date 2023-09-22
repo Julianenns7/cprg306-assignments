5a4548f60688dd03023900db9e7e40c3f1a2fa87
Julian Enns <julianenns7@gmail.com>
2023-09-22 16:30:52 -0600

    CPRG306-Assignments from week2

---
+++ app/my_components.js
@@ -0,0 +1,12 @@
+
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+            <main> 
+                <h2> Julian Enns</h2>
+                <h3>CPRG 306 E</h3>
+                <Link href= "https://github.com/Julianenns7/cprg306-assignments">My repository </Link>
+            </main>
+        )   
+    }
---
+++ app/week2/page.js
@@ -0,0 +1,11 @@
+
+import StudentInfo from "../my_components";
+
+export default function Week2(){
+    return (
+        <main>
+            <h1>My Shopping List</h1>
+            <StudentInfo />
+        </main>
+    )
+}
